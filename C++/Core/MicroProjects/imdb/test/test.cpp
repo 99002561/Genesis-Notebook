ae3b034fc18c3fce7a55d0ecf94e33313b16f74a
Naga Akhil ES <nagaakhil.es@ltts.com>
2020-10-25 15:35:25 +0530

    MicroProjects

---
+++ C++/Core/MicroProjects/imdb/test/test.cpp
@@ -0,0 +1,15 @@
+
+#include "imdbTest.h"
+
+/*
+#include "imdb.cpp"
+//#include "imdbDB.cpp"
+#include "movies.cpp"*/
+
+
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
